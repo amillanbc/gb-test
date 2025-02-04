085fc43f5a5d697bc24f7a7c3c21e20df7adfb0f
amillanbc <amillan@bluecorela.com>
2025-02-04 16:33:33 -0500

    toggle snippets

--- src/app/views/global-bank-components/toggle/toggle.page.ts
+++ src/app/views/global-bank-components/toggle/toggle.page.ts
@@ -5,14 +5,25 @@ import { IonContent, IonRow, IonCol } from '@ionic/angular/standalone';
 // ##### GB COMPONENTS
 import { GbToggleComponent } from 'src/app/components/global/gb-toggle/gb-toggle.component';
 
+// ##### OTHER IMPORTS
+import { Highlight } from 'ngx-highlightjs';
+
 @Component({
   selector: 'app-toggle',
   templateUrl: './toggle.page.html',
   styleUrls: ['./toggle.page.scss'],
   standalone: true,
-  imports: [IonContent, IonRow, IonCol, GbToggleComponent],
+  imports: [IonContent, IonRow, IonCol, GbToggleComponent, Highlight],
 })
 export class TogglePage {
   // ##### SIGNALS
   isToggled = signal(false);
+
+  primary = '<gb-toggle name="toggle_1" [(value)]="isToggled" />';
+  label =
+    '<gb-toggle name="toggle_2" [(value)]="isToggled" label="Toggle label" />';
+  labelPosition =
+    '<gb-toggle name="toggle_3" [(value)]="isToggled" label="Toggle label" labelPosition="right" />';
+  color =
+    '<gb-toggle name="toggle_4" [(value)]="isToggled" color="blue" />\n<gb-toggle name="toggle_5" [(value)]="isToggled" color="pink" />';
 }
