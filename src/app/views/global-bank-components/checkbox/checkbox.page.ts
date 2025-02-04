264f185cd103b90616e4c5dcd156220025dfcddd
amillanbc <amillan@bluecorela.com>
2025-02-04 15:14:44 -0500

    checkbox snippets

--- src/app/views/global-bank-components/checkbox/checkbox.page.ts
+++ src/app/views/global-bank-components/checkbox/checkbox.page.ts
@@ -1,18 +1,28 @@
 // ##### IONIC & ANGULAR
 import { Component, signal } from '@angular/core';
-import { IonContent } from '@ionic/angular/standalone';
+import { IonContent, IonRow, IonCol } from '@ionic/angular/standalone';
 
 // ##### GB COMPONENTS
 import { GbCheckboxComponent } from 'src/app/components/global/gb-checkbox/gb-checkbox.component';
+
+// ##### OTHER IMPORTS
+import { Highlight } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-checkbox',
   templateUrl: './checkbox.page.html',
   styleUrls: ['./checkbox.page.scss'],
   standalone: true,
-  imports: [IonContent, GbCheckboxComponent],
+  imports: [IonContent, GbCheckboxComponent, Highlight, IonRow, IonCol],
 })
 export class CheckboxPage {
   // ##### SIGNALS
   isChecked = signal(false);
+
+  primary =
+    '<gb-checkbox [(value)]="isChecked" name="checkbox_1" label="Checkbox label" />';
+  colors =
+    '<gb-checkbox [(value)]="isChecked" name="checkbox_2" label="Checkbox label" />\n<gb-checkbox [(value)]="isChecked" name="checkbox_3" label="Checkbox label" color="pink" />';
+  disabled =
+    '<gb-checkbox [(value)]="isChecked" name="checkbox_4" label="Checkbox label" [disabled]="true" />';
 }
