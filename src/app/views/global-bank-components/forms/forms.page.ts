bcd85fdcd94fa2e15c593c22a636374c7b1e2df3
amillanbc <amillan@bluecorela.com>
2025-02-08 21:53:13 -0500

    forms snippets

--- src/app/views/global-bank-components/forms/forms.page.ts
+++ src/app/views/global-bank-components/forms/forms.page.ts
@@ -1,3 +1,4 @@
+// ##### IONIC & ANGULAR
 import { Component, signal, computed, inject } from '@angular/core';
 import { IonContent, IonRow, IonCol } from '@ionic/angular/standalone';
 
@@ -8,6 +9,9 @@ import { GbCheckboxComponent } from 'src/app/components/global/gb-checkbox/gb-ch
 
 // ##### SERVICES
 import { Utils } from 'src/app/stores/utils.service';
+
+// ##### OTHER IMPORTS
+import { Highlight } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-forms',
@@ -21,6 +25,7 @@ import { Utils } from 'src/app/stores/utils.service';
     GbInputComponent,
     GbBtnComponent,
     GbCheckboxComponent,
+    Highlight,
   ],
 })
 export class FormsPage {
@@ -50,11 +55,90 @@ export class FormsPage {
 
   // ##### METHODS
   submitLogin() {
-    alert('loging in!!!');
+    alert('Registration successfull');
   }
 
   // ##### COMPUTED
   isFormValid = computed(() => {
     return this.utils.validateForm(this.formData);
   });
+
+  html = `
+  <gb-input
+    placeholder="User"
+    [(value)]="formData.user.value"
+    [regex]="formData.user.validator"
+    [required]="true"
+    icon="person-outline" />
+
+  <gb-input
+    placeholder="Password"
+    [(value)]="formData.pass.value"
+    [regex]="formData.pass.validator"
+    icon="lock-closed-outline"
+    [required]="true"
+    type="password" />
+
+  <gb-input
+    placeholder="Age"
+    [(value)]="formData.age.value"
+    type="number"
+    [required]="true"
+    [min]="formData.age.min"
+    [max]="formData.age.max" />
+
+  <gb-checkbox
+    name="checkbox_1"
+    [(value)]="formData.accept.value"
+    label="Accept terms" />
+
+  <gb-btn
+    label="Register"
+    (click)="submitLogin()"
+    [disabled]="!isFormValid()" />`;
+
+  ts = `
+  // ##### IONIC & ANGULAR
+  import { signal, computed, inject } from '@angular/core';
+
+  // ##### SERVICES
+  import { Utils } from 'src/app/stores/utils.service';
+
+  export class FormsPage {
+    // ##### INJECTIONS
+    utils = inject(Utils);
+
+    // ##### SIGNALS
+    formData = {
+      user: {
+        value: signal(''),
+        validator: '^.{3,}$',
+      },
+      pass: {
+        value: signal(''),
+        validator: '^.{3,}$',
+      },
+      age: {
+        value: signal(''),
+        min: 0,
+        max: 50,
+      },
+      accept: {
+        value: signal(false),
+        validator: true,
+      },
+    };
+    
+    // ##### METHODS
+    submitLogin() {
+      // your code here
+    }
+      
+    // ##### COMPUTED
+    isFormValid = computed(() => {
+      return this.utils.validateForm(this.formData);
+    });
+  }
+
+  `;
 }
