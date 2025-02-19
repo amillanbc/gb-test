73c7d691fa787f6fa89d508d5058ae7472d5762d
amillanbc <amillan@bluecorela.com>
2025-02-19 14:31:34 -0500

    full screen modal close btn

--- src/app/components/modal-sample/modal-sample.component.ts
+++ src/app/components/modal-sample/modal-sample.component.ts
@@ -1,12 +1,25 @@
 // ##### IONIC & ANGULAR
 import { Component, inject } from '@angular/core';
 import { ModalController } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonButton,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-modal-sample',
   templateUrl: './modal-sample.component.html',
   styleUrls: ['./modal-sample.component.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonButtons, IonButton],
 })
 export class ModalSampleComponent {
   modalCtrl = inject(ModalController);
+
+  // ##### METHODS
+  closeModal() {
+    this.modalCtrl.dismiss();
+  }
 }
