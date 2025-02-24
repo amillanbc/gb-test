b311b4f507eec5cc82b75b4ffda639cb2efc9e13
amillanbc <amillan@bluecorela.com>
2025-02-24 11:20:30 -0500

    progress bar page snippets

--- src/app/views/global-bank-components/progress-bar/progress-bar.page.ts
+++ src/app/views/global-bank-components/progress-bar/progress-bar.page.ts
@@ -1,20 +1,42 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+// ##### IONIC & ANGULAR
+import { Component, OnInit, signal } from '@angular/core';
+import { IonContent, IonRow, IonCol } from '@ionic/angular/standalone';
+
+// ##### GB COMPONENT
+import { GbProgressBarComponent } from 'src/app/components/global/gb-progress-bar/gb-progress-bar.component';
+
+// ##### OTHER IMPORTS
+import { Highlight } from 'ngx-highlightjs';
 
 @Component({
   selector: 'app-progress-bar',
   templateUrl: './progress-bar.page.html',
   styleUrls: ['./progress-bar.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCol, IonRow, IonContent, GbProgressBarComponent, Highlight]
 })
 export class ProgressBarPage implements OnInit {
+  // ##### SIGNALS
+  progress = signal(0)
 
-  constructor() { }
-
-  ngOnInit() {
+  // METHODS
+  loopCount() {
+    let count = 0;
+    setInterval(() => {
+      this.progress.update(val => val = count)
+      count = (count + 1) % 101;
+    }, 25);
   }
+
+  // LC HOOKS
+  ngOnInit() {
+    this.loopCount()
+  }
+
+  default = `<gb-progress-bar [progress]="100" />`
+
+  color = `<gb-progress-bar [progress]="75" color="pink" />`
+
+  level = `<gb-progress-bar [progress]="50" color="green" [level]="300" />`
 
 }
