96abe5a5381c8935d1c76acefb595ef3bbe86dc3
amillanbc <amillan@bluecorela.com>
2025-02-25 14:05:44 -0500

    utils form validator refactor

--- src/app/stores/utils.service.ts
+++ src/app/stores/utils.service.ts
@@ -38,26 +38,38 @@ export class Utils {
   validateForm(formData: FormObject) {
     let isValid = true;
     for (let field in formData) {
-      const val = formData[field].value();
-      const validator = formData[field].validator;
-      const min = formData[field].min;
-      const max = formData[field].max;
+      const [val, validator, min, max] = [
+        formData[field].value(),
+        formData[field].validator,
+        formData[field].min,
+        formData[field].max
+      ]
       const num = parseFloat(`${val}`);
-      if (typeof validator == 'boolean' && val != validator) isValid = false;
-      if (typeof val == 'string') {
-        if (typeof validator == 'string') {
-          const regex = new RegExp(validator);
-          if (typeof val == 'string' && !regex.test(val)) isValid = false;
-        }
-        if (min || max) {
-          if (isNaN(num)) isValid = false;
-          else {
-            if (min != undefined && num < min) isValid = false;
-            if (max != undefined && num > max) isValid = false;
-          }
-        }
+      if (typeof validator === 'boolean' && val !== validator) isValid = false;
+      if (typeof val === 'string' && typeof validator === 'string') {
+        isValid = this.validateString(validator, val)
+      }
+      if (typeof val === 'string' && (min || max)) {
+        isValid = this.validateMinMax(min, max, num)
       }
     }
     return isValid;
+  }
+
+  validateString(validator: string, val: string): boolean {
+    let isValid = true
+    const regex = new RegExp(validator);
+    if (typeof val === 'string' && !regex.test(val)) isValid = false;
+    return isValid
+  }
+
+  validateMinMax(min: number | undefined, max: number | undefined, num: number): boolean {
+    let isValid = true
+    if (isNaN(num)) isValid = false;
+    else {
+      if (typeof min !== "undefined" && num < min) isValid = false;
+      if (typeof max !== "undefined" && num > max) isValid = false;
+    }
+    return isValid
   }
 }
