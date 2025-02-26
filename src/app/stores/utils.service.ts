d85f64cb9be4cb146658c2f3843ae6b264185510
amillanbc <amillan@bluecorela.com>
2025-02-26 11:42:21 -0500

    form validation refactor

--- src/app/stores/utils.service.ts
+++ src/app/stores/utils.service.ts
@@ -23,7 +23,6 @@ export class Utils {
     fullscreen?: boolean;
     comp?: any;
   }) {
-    // props: object, fullscreen: boolean = false, comp?: any
     const modal = await this.modalCtrl.create({
       component: comp || GbGenericModalComponent,
       id: fullscreen ? '' : 'dialog-modal',
@@ -32,11 +31,10 @@ export class Utils {
     modal.present();
     const { data } = await modal.onWillDismiss();
     if (data) return data.action;
-    return undefined;
+    return null;
   }
 
   validateForm(formData: FormObject) {
-    let isValid = true;
     for (let field in formData) {
       const [val, validator, min, max] = [
         formData[field].value(),
@@ -45,31 +43,42 @@ export class Utils {
         formData[field].max
       ]
       const num = parseFloat(`${val}`);
-      if (typeof validator === 'boolean' && val !== validator) isValid = false;
-      if (typeof val === 'string' && typeof validator === 'string') {
-        isValid = this.validateString(validator, val)
-      }
-      if (typeof val === 'string' && (min || max)) {
-        isValid = this.validateMinMax(min, max, num)
+      if (typeof validator === 'boolean' && val !== validator) return false;
+      if (typeof validator === 'string' || min || max) {
+        if (!this.validateField(val, validator, min, max, num)) return false
       }
     }
-    return isValid;
+    return true;
   }
 
-  validateString(validator: string, val: string): boolean {
-    let isValid = true
+  validateField(
+    val: string | boolean,
+    validator: string | boolean | undefined,
+    min: number | undefined,
+    max: number | undefined,
+    num: number
+  ): boolean {
+    if (typeof val === 'string' && typeof validator === 'string') {
+      if (!this.validateString(validator, val)) return false
+   }
+   if (typeof val === 'string' && (min || max)) {
+     if (!this.validateMinMax(min, max, num)) return false
+   }
+   return true
+  }
+
+  private validateString(validator: string, val: string): boolean {
     const regex = new RegExp(validator);
-    if (typeof val === 'string' && !regex.test(val)) isValid = false;
-    return isValid
+    if (typeof val === 'string' && !regex.test(val)) return false
+    return true
   }
 
-  validateMinMax(min: number | undefined, max: number | undefined, num: number): boolean {
-    let isValid = true
-    if (isNaN(num)) isValid = false;
+  private validateMinMax(min: number | undefined, max: number | undefined, num: number): boolean {
+    if (isNaN(num)) return false
     else {
-      if (typeof min !== "undefined" && num < min) isValid = false;
-      if (typeof max !== "undefined" && num > max) isValid = false;
+      if (typeof min !== "undefined" && num < min) return false
+      if (typeof max !== "undefined" && num > max) return false
     }
-    return isValid
+    return true
   }
 }
