6f7dae31318a8eee8b8a8caa3ab9e64a42e94748
amillanbc <amillan@bluecorela.com>
2025-03-07 11:42:54 -0500

    gb icon component

---
+++ src/app/components/global/gb-icon/gb-icon.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gb-icon',
+  templateUrl: './gb-icon.component.html',
+  styleUrls: ['./gb-icon.component.scss'],
+})
+export class GbIconComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
